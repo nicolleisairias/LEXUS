43f938d9a60b657bc7e4b8630460023ee4bd1348
Alejandris <159916209+Alejandris@users.noreply.github.com>
2025-02-25 04:32:06 +0000

    Autentificacion

---
+++ src/app/Componentes/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { UserService } from '../../Service/user.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: ``
+})
+export class LoginComponent implements OnInit
+{
+  formLogin: FormGroup;
+
+  constructor(
+     private userService : UserService,
+     private router : Router
+  ){
+    this.formLogin = new FormGroup({
+      email : new FormControl(),
+      password: new FormControl()
+    })
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit(){
+    this.userService.login(this.formLogin.value)
+    .then(response =>{
+      console.log(response);
+      this.router.navigate(['/inicio']);
+    })
+    .catch(error => console.log(error));
+    
+  }
+
+  onClick(){
+    this.userService.loginWithGoogle()
+    .then(response =>{
+      console.log(response);
+      this.router.navigate(['/inicio']);
+    })
+    .catch(error => console.log(error));
+
+}
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,42 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeAdministradorComponent } from './pages/home-administrador/home-administrador.component';
-
+import { canActivate, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { RegisterComponent } from './Componentes/register/register.component';
+import { LoginComponent } from './Componentes/login/login.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-  { path: 'inicio', component: InicioComponent },
-  {
-    path: 'admin',
-    children: [
-      { path: 'home', component: HomeAdministradorComponent }
-    ]
+  { path: '', pathMatch: 'full', redirectTo: '/inicio' },
+  { path: 'inicio', component: InicioComponent, 
+  ...canActivate(() => redirectUnauthorizedTo(['/register']))
   },
   {
-    path: 'cliente',
-    loadChildren: () => import('../app/pages/home-cliente/cliente.module')
-      .then(m => m.ClienteModule)
+    path: 'register',component: RegisterComponent
   },
   {
-    path: 'empleado',
-    loadChildren:() => import('../app/pages/home-empleado/empleado.module')
-    .then(m => m.EmpleadoModule)
+    path: 'login', component: LoginComponent
   },
-  { path: '**', redirectTo: '/inicio' }
+  // {
+  //   path: 'admin',
+  //   children: [
+  //     { path: 'home', component: HomeAdministradorComponent }
+  //   ]
+  // },
+
+ 
+  // {
+  //   path: 'cliente',
+  //   loadChildren: () => import('../app/pages/home-cliente/cliente.module')
+  //     .then(m => m.ClienteModule)
+  // },
+  // {
+  //   path: 'empleado',
+  //   loadChildren:() => import('../app/pages/home-empleado/empleado.module')
+  //   .then(m => m.EmpleadoModule)
+  // },
+  
 ];
 
 @NgModule({
